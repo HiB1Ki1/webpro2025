61f967157a2e0f285049b48a6d8eedb3278a06ed
HiB1Ki1 <hibikiabe2023256@keio.jp>
2025-07-09 11:50:42 +0900

    fix: package.jsonの依存関係を修正

--- prisma/seed.js
+++ prisma/seed.js
@@ -1,41 +1,47 @@
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
-async function main() {
-  // 既存のデータをすべて削除
-  await prisma.restaurant.deleteMany({});
-  console.log("Deleted existing restaurants.");
+// 登録したいお店のデータ
+const restaurantsToSeed = [
+  {
+    name: "ラーメン ぶっ豚 日吉店",
+    genre: "ラーメン",
+    price_range: "1000円〜2000円",
+    address: "神奈川県横浜市港北区日吉本町1-2-6",
+    latitude: 35.55328,
+    longitude: 139.64627,
+  },
+  {
+    name: "壱角家 日吉店",
+    genre: "ラーメン",
+    price_range: "〜1000円",
+    address: "神奈川県横浜市港北区日吉2-1-8",
+    latitude: 35.5524,
+    longitude: 139.64805,
+  },
+  {
+    name: "武蔵家 日吉店",
+    genre: "ラーメン",
+    price_range: "〜1000円",
+    address: "神奈川県横浜市港北区日吉本町1-3-19",
+    latitude: 35.55331,
+    longitude: 139.6475,
+  },
+];
 
-  // 新しいデータを作成
-  const newRestaurants = await prisma.restaurant.createMany({
-    data: [
-      {
-        name: "ラーメン ぶっ豚 日吉店",
-        genre: "ラーメン",
-        price_range: "1000円〜2000円",
-        address: "神奈川県横浜市港北区日吉本町1-2-6",
-        latitude: 35.55328,
-        longitude: 139.64627,
-      },
-      {
-        name: "壱角家 日吉店",
-        genre: "ラーメン",
-        price_range: "〜1000円",
-        address: "神奈川県横浜市港北区日吉2-1-8",
-        latitude: 35.5524,
-        longitude: 139.64805,
-      },
-      {
-        name: "武蔵家 日吉店",
-        genre: "ラーメン",
-        price_range: "〜1000円",
-        address: "神奈川県横浜市港北区日吉本町1-3-19",
-        latitude: 35.55331,
-        longitude: 139.6475,
-      },
-    ],
-  });
-  console.log(`Created ${newRestaurants.count} new restaurants.`);
+async function main() {
+  console.log("Start seeding...");
+  for (const r of restaurantsToSeed) {
+    const restaurant = await prisma.restaurant.upsert({
+      where: { address: r.address }, // 住所をユニークなキーとしてお店を探す
+      update: {}, // もし見つかった場合、何もしない（情報を更新したければここに書く）
+      create: r, // もし見つからなかった場合、新しいデータとして作成する
+    });
+    console.log(
+      `Created or found restaurant with address: ${restaurant.address}`
+    );
+  }
+  console.log("Seeding finished.");
 }
 
 main()
